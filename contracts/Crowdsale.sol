470b301ffb230a709d8653f868b4c0d924ad68c4
PillarDevelopment <2622610@gmail.com>
2018-05-10 12:26:36 +0300

    Изменена версия компилятора добавлены модификаторы событий emit Добавлен оператор backEnd для авторизации участников WL

--- contracts/Crowdsale.sol
+++ contracts/Crowdsale.sol
@@ -1,4 +1,4 @@
-pragma solidity ^0.4.18;
+pragma solidity ^0.4.21;
 
 
 library SafeMath {
@@ -27,8 +27,8 @@ library SafeMath {
 
 
 interface ERC20 {
-  function transfer (address _beneficiary, uint256 _tokenAmount) external returns (bool);  
-  function sendToSaleAgent (address _saleAgent, uint256 _value) external;
+    function transfer (address _beneficiary, uint256 _tokenAmount) external returns (bool);
+    function sendToSaleAgent (address _saleAgent, uint256 _value) external;
 }
 
 
@@ -45,223 +45,238 @@ contract Ownable {
 
 
 contract Crowdsale is Ownable {
-  using SafeMath for uint256;
+    using SafeMath for uint256;
 
-  modifier onlyWhileOpen {
-      require(now >= startDate && now < endDate);
-      _;
-  }
-
-  // The token being sold
-  ERC20 public token;
-
-  // Address where funds are collected
-  address public wallet;
-
-  // Сколько токенов покупатель получает за 1 эфир
-  uint256 public rate;
-
-  // Amount of wei raised
-  uint256 public weiRaised;
-
-  // Потолок привлечения средств в ходе ICO в $
-  uint256 public hardcap;
-
-  // Цена ETH в $
-  uint256 public ETHUSD;
-
-  // Дата начала
-  uint256 public startDate;
-
-  // Дата окончания
-  uint256 public endDate;
-
-  // Бонус за этап, %
-  uint8 public stageBonus;
-
-  // Является ли текущий этап - PreICO
-  bool public isPreICO;
-
-  // Завершился ли этап
-  bool public isFinalized = false;
-
-  // Whitelist
-  mapping(address => bool) public whitelist;
-
-  event TokenPurchase(address indexed purchaser, address indexed beneficiary, uint256 value, uint256 amount);
-  event Finalized();
-
-  function Crowdsale(
-    uint256 _rate, 
-    address _wallet, 
-    uint256 _startDate, 
-    uint256 _endDate,
-    bool _isPreICO, 
-    uint256 _hardcap,
-    uint8 _stageBonus
-  ) public {
-    require(_rate > 0);
-    require(_endDate >= _startDate);
-    require(_hardcap > 0);
-
-    rate = _rate;
-    wallet = _wallet;
-    startDate = _startDate;
-    endDate = _endDate;
-    isPreICO = _isPreICO;
-    hardcap = _hardcap;
-
-    if(isPreICO){
-      stageBonus = 30;
-    }else{
-      stageBonus = _stageBonus;
-    }
-  }
-
-  // Установить кошелек для сбора средств
-  function setWallet (uint16 _wallet) public onlyOwner {
-    wallet = _wallet;
-  }
-
-  // Установить стоимость токена
-  function setRate (uint16 _rate) public onlyOwner {
-    require(_rate > 0);
-    rate = _rate;
-  }
-
-  // Установить торгуемй токен
-  function setToken (ERC20 _token) public onlyOwner {
-    token = _token;
-  }
-  
-  // Установить hardcap в $
-  function setHardcap (uint256 _hardcap) public onlyOwner {
-    hardcap = _hardcap;
-  }
-  
-
-  // Установить дату начала
-  function setStartDate (uint256 _startDate) public onlyOwner {
-    require(_startDate < endDate);
-    startDate = _startDate;
-  }
-
-  // Установить дату окончания
-  function setEndDate (uint256 _endDate) public onlyOwner {
-    require(_endDate > startDate);
-    endDate = _endDate;
-  }
-
-  // Установить стоимость эфира в центах
-  function setETHUSD (uint256 _ETHUSD) public onlyOwner {
-    ETHUSD = _ETHUSD;
-  }
-
-  // Получить токены для продажи
-  function getTokens (uint256 _amount) public onlyOwner {
-    token.sendToSaleAgent(this, _amount);
-  }
-
-  function () external payable {
-    buyTokens(msg.sender);
-  }
-
-  // Покупка токенов
-  function buyTokens(address _beneficiary) public payable {
-
-    uint256 weiAmount = msg.value;
-    _preValidatePurchase(_beneficiary, weiAmount);
-
-    // Считаем сколько токенов перевести
-    uint256 tokens = _getTokenAmountWithBonus(weiAmount);
-
-    // update state
-    weiRaised = weiRaised.add(weiAmount);
-
-    _deliverTokens(_beneficiary, tokens);
-
-    TokenPurchase(msg.sender, _beneficiary, weiAmount, tokens);
-
-    _forwardFunds();
-  }
-
-  // Добавить адрес в whitelist
-  function addToWhitelist(address _beneficiary) public onlyOwner {
-    whitelist[_beneficiary] = true;
-  }
-
-  // Добавить несколько адресов в whitelist
-  function addManyToWhitelist(address[] _beneficiaries) public onlyOwner {
-    for (uint256 i = 0; i < _beneficiaries.length; i++) {
-      whitelist[_beneficiaries[i]] = true;
-    }
-  }
-
-  // Исключить адрес из whitelist
-  function removeFromWhitelist(address _beneficiary) external onlyOwner {
-    whitelist[_beneficiary] = false;
-  }
-
-  // Узнать истек ли срок проведения
-  function hasClosed() public view returns (bool) {
-    return now > endDate;
-  }
-
-  // Завершить этап ICO
-  function finalize() onlyOwner public {
-      require(!isFinalized);
-      require(hasClosed());
-
-      Finalized();
-      isFinalized = true;
-  }
-
-  /*
-   * Внутренние методы
-   */
-
-   // Валидация перед покупкой токенов
-  function _preValidatePurchase(address _beneficiary, uint256 _weiAmount) internal view onlyWhileOpen {
-    require(!isFinalized);
-    require(_beneficiary != address(0));
-    require(_weiAmount != 0);
-    require(weiRaised.add(_weiAmount).mul(ETHUSD).div(10**18) <= hardcap);
-    require(!isPreICO || (isPreICO && whitelist[_beneficiary]));
-  }
-
-  // Подсчет количества токенов в зависимости от суммы платежа и бонусных программ
-  function _getTokenAmountWithBonus(uint256 _weiAmount) internal view returns(uint256) {
-    uint256 baseTokenAmount = _weiAmount.mul(rate);
-    uint256 tokenAmount = baseTokenAmount;
-    uint256 usdAmount = _weiAmount.mul(ETHUSD).div(10**18);
-
-    // Считаем бонусы за объем инвестиций, если это не этап PreICO
-    if(!isPreICO){
-      if(usdAmount >= 200000){
-        tokenAmount = tokenAmount.add(baseTokenAmount.mul(10).div(100));
-      } else if(usdAmount >= 50000){
-        tokenAmount = tokenAmount.add(baseTokenAmount.mul(7).div(100));
-      } else if(usdAmount >= 20000){
-        tokenAmount = tokenAmount.add(baseTokenAmount.mul(5).div(100));
-      } else if(usdAmount >= 5000){
-        tokenAmount = tokenAmount.add(baseTokenAmount.mul(3).div(100));
-      }
+    modifier onlyWhileOpen {
+        require(now >= startDate && now < endDate);
+        _;
     }
 
-    // Считаем бонусы за этап ICO
-    if(stageBonus > 0){
-      tokenAmount = tokenAmount.add(baseTokenAmount.mul(stageBonus).div(100));
+    // The token being sold
+    ERC20 public token;
+
+    // Address where funds are collected
+    address public wallet;
+
+    // Адрес оператора бекЭнда для управления вайтлистом
+    address public backendOperator = msg.sender;
+
+    // Сколько токенов покупатель получает за 1 эфир
+    uint256 public rate;
+
+    // Amount of wei raised
+    uint256 public weiRaised;
+
+    // Потолок привлечения средств в ходе ICO в $
+    uint256 public hardcap;
+
+    // Цена ETH в $
+    uint256 public ETHUSD;
+
+    // Дата начала
+    uint256 public startDate;
+
+    // Дата окончания
+    uint256 public endDate;
+
+    // Бонус за этап, %
+    uint8 public stageBonus;
+
+    // Является ли текущий этап - PreICO
+    bool public isPreICO;
+
+    // Завершился ли этап
+    bool public isFinalized = false;
+
+    // Whitelist
+    mapping(address => bool) public whitelist;
+
+    event TokenPurchase(address indexed purchaser, address indexed beneficiary, uint256 value, uint256 amount);
+    event Finalized();
+
+    function Crowdsale(
+        uint256 _rate,
+        address _wallet,
+        uint256 _startDate,
+        uint256 _endDate,
+        bool _isPreICO,
+        uint256 _hardcap,
+        uint8 _stageBonus
+    ) public {
+        require(_rate > 0);
+        require(_endDate >= _startDate);
+        require(_hardcap > 0);
+
+        rate = _rate;
+        wallet = _wallet;
+        startDate = _startDate;
+        endDate = _endDate;
+        isPreICO = _isPreICO;
+        hardcap = _hardcap;
+
+        if(isPreICO){
+            stageBonus = 30;
+        }else{
+            stageBonus = _stageBonus;
+        }
     }
-    return tokenAmount;
-  }
 
-  // Перевод токенов
-  function _deliverTokens(address _beneficiary, uint256 _tokenAmount) internal {
-    token.transfer(_beneficiary, _tokenAmount);
-  }
+    modifier backEnd() {
+        require(msg.sender == backendOperator || msg.sender == owner);
+        _;
+    }
 
-  // Перевод средств на кошелек компании
-  function _forwardFunds() internal {
-    wallet.transfer(msg.value);
-  }
+
+    // Установить кошелек для сбора средств
+    function setWallet (uint16 _wallet) public onlyOwner {
+        wallet = _wallet;
+    }
+
+    // Установить стоимость токена
+    function setRate (uint16 _rate) public onlyOwner {
+        require(_rate > 0);
+        rate = _rate;
+    }
+
+    // Установить торгуемй токен
+    function setToken (ERC20 _token) public onlyOwner {
+        token = _token;
+    }
+
+    // Установить hardcap в $
+    function setHardcap (uint256 _hardcap) public onlyOwner {
+        hardcap = _hardcap;
+    }
+
+
+    // Установить дату начала
+    function setStartDate (uint256 _startDate) public onlyOwner {
+        require(_startDate < endDate);
+        startDate = _startDate;
+    }
+
+    // Установить дату окончания
+    function setEndDate (uint256 _endDate) public onlyOwner {
+        require(_endDate > startDate);
+        endDate = _endDate;
+    }
+
+    // Установить стоимость эфира в центах
+    function setETHUSD (uint256 _ETHUSD) public onlyOwner {
+        ETHUSD = _ETHUSD;
+    }
+
+    // Установить оператора БекЭнда для управления вайтлистом
+    function setBackendOperator(address newOperator) public onlyOwner {
+        backendOperator = newOperator;
+    }
+
+
+    // Получить токены для продажи
+    function getTokens (uint256 _amount) public onlyOwner {
+        token.sendToSaleAgent(this, _amount);
+    }
+
+    function () external payable {
+        buyTokens(msg.sender);
+    }
+
+    // Покупка токенов
+    function buyTokens(address _beneficiary) public payable {
+
+        uint256 weiAmount = msg.value;
+        _preValidatePurchase(_beneficiary, weiAmount);
+
+        // Считаем сколько токенов перевести
+        uint256 tokens = _getTokenAmountWithBonus(weiAmount);
+
+        // update state
+        weiRaised = weiRaised.add(weiAmount);
+
+        _deliverTokens(_beneficiary, tokens);
+
+        emit TokenPurchase(msg.sender, _beneficiary, weiAmount, tokens);
+
+        _forwardFunds();
+    }
+
+    // Добавить адрес в whitelist
+    function addToWhitelist(address _beneficiary) public backEnd {
+        whitelist[_beneficiary] = true;
+    }
+
+    // Добавить несколько адресов в whitelist
+    function addManyToWhitelist(address[] _beneficiaries) public backEnd {
+        for (uint256 i = 0; i < _beneficiaries.length; i++) {
+            whitelist[_beneficiaries[i]] = true;
+        }
+    }
+
+    // Исключить адрес из whitelist
+    function removeFromWhitelist(address _beneficiary) external backEnd {
+        whitelist[_beneficiary] = false;
+    }
+
+    // Узнать истек ли срок проведения
+    function hasClosed() public view returns (bool) {
+        return now > endDate;
+    }
+
+    // Завершить этап ICO
+    function finalize() onlyOwner public {
+        require(!isFinalized);
+        require(hasClosed());
+
+        emit Finalized();
+        isFinalized = true;
+    }
+
+    /*
+     * Внутренние методы
+     */
+
+    // Валидация перед покупкой токенов
+    function _preValidatePurchase(address _beneficiary, uint256 _weiAmount) internal view onlyWhileOpen {
+        require(!isFinalized);
+        require(_beneficiary != address(0));
+        require(_weiAmount != 0);
+        require(weiRaised.add(_weiAmount).mul(ETHUSD).div(10**18) <= hardcap);
+        require(!isPreICO || (isPreICO && whitelist[_beneficiary]));
+    }
+
+    // Подсчет количества токенов в зависимости от суммы платежа и бонусных программ
+    function _getTokenAmountWithBonus(uint256 _weiAmount) internal view returns(uint256) {
+        uint256 baseTokenAmount = _weiAmount.mul(rate);
+        uint256 tokenAmount = baseTokenAmount;
+        uint256 usdAmount = _weiAmount.mul(ETHUSD).div(10**18);
+
+        // Считаем бонусы за объем инвестиций, если это не этап PreICO
+        if(!isPreICO){
+            if(usdAmount >= 200000){
+                tokenAmount = tokenAmount.add(baseTokenAmount.mul(10).div(100));
+            } else if(usdAmount >= 50000){
+                tokenAmount = tokenAmount.add(baseTokenAmount.mul(7).div(100));
+            } else if(usdAmount >= 20000){
+                tokenAmount = tokenAmount.add(baseTokenAmount.mul(5).div(100));
+            } else if(usdAmount >= 5000){
+                tokenAmount = tokenAmount.add(baseTokenAmount.mul(3).div(100));
+            }
+        }
+
+        // Считаем бонусы за этап ICO
+        if(stageBonus > 0){
+            tokenAmount = tokenAmount.add(baseTokenAmount.mul(stageBonus).div(100));
+        }
+        return tokenAmount;
+    }
+
+    // Перевод токенов
+    function _deliverTokens(address _beneficiary, uint256 _tokenAmount) internal {
+        token.transfer(_beneficiary, _tokenAmount);
+    }
+
+    // Перевод средств на кошелек компании
+    function _forwardFunds() internal {
+        wallet.transfer(msg.value);
+    }
 }
