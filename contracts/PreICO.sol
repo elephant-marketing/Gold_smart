21245b47b751f5df530f05e82892c0ac9b6250e1
PillarDevelopment <2622610@gmail.com>
2018-05-24 17:33:59 +0300

    update

--- contracts/PreICO.sol
+++ contracts/PreICO.sol
@@ -31,7 +31,7 @@ library SafeMath {
 
 interface ERC20 {
     function transfer (address _beneficiary, uint256 _tokenAmount) external returns (bool);
-    function transferFromICO(address _to, uint256 _value) external returns(bool);
+    function mintFromICO(address _to, uint256 _amount) external  returns(bool);
 }
 /**
  * @title Ownable
@@ -54,33 +54,35 @@ contract Ownable {
  * @title CrowdSale
  * @dev https://github.com/
  */
-contract CloseICO is Ownable {
+contract SuperSafeSale is Ownable {
 
     ERC20 public token;
 
     using SafeMath for uint;
 
     address public backEndOperator = msg.sender;
-    address team = 0x7eDE8260e573d3A3dDfc058f19309DF5a1f7397E; // 33 % - команда и первичные инвесторы
-    address bounty = 0x0cdb839B52404d49417C8Ded6c3E2157A06CdD37; // 2 % - для баунти программы
-    address reserve = 0xC032D3fCA001b73e8cC3be0B75772329395caA49; // 5% consult
+    address team = 0x7eDE8260e573d3A3dDfc058f19309DF5a1f7397E; // 33% - основантели проекта
+    address bounty = 0x0cdb839B52404d49417C8Ded6c3E2157A06CdD37; // 2% - для баунти программы
+    address reserve; // 3%
 
     mapping(address=>bool) public whitelist;
 
     mapping(address => uint256) public investedEther;
 
-    uint256 public startCloseSale = now;
-    uint256 public endCloseSale = startCloseSale + 1814400; // 3 weeks
+    uint256 public startPreSale = 1531612801; // Sunday, 15-Jul-18 00:00:01 UTC
+
+    uint256 public endPreSale = 1534377599; // Wednesday, 15-Aug-18 23:59:59 UTC i
 
     uint256 public investors; // общее количество инвесторов
     uint256 public weisRaised;
 
-    uint256 public hardCapCloseSale = 6400000*1e18; // 6,400,000 FBW
+    uint256 public softCapPreSale = 2700000*1e18; // именно на преварительном
+    uint256 public hardCapPreSale = 15000000*1e18; //  именно на преварительном
 
-    uint256 public buyPrice; // 0.4 USD
+    uint256 public buyPrice; // 0.14 USD
     uint256 public dollarPrice;
 
-    uint256 public soldTokensCloseSale;
+    uint256 public soldTokens;
 
     event Authorized(address wlCandidate, uint timestamp);
     event Revoked(address wlCandidate, uint timestamp);
@@ -95,17 +97,27 @@ contract CloseICO is Ownable {
     constructor(ERC20 _token, uint256 usdETH) public {
         token = _token;
         dollarPrice = usdETH;
-        buyPrice = (1e17/dollarPrice)*4; // 0.4 usd
+        buyPrice = (1e17/dollarPrice)*21; // 2.10 usd
     }
 
     // изменение даты начала предварительной распродажи
-    function setStartCloseSale(uint256 newStartCloseSale) public onlyOwner {
-        startCloseSale = newStartCloseSale;
+    function setStartPreSale(uint256 newStartPreSale) public onlyOwner {
+        startPreSale = newStartPreSale;
     }
 
     // изменение даты окончания предварительной распродажи
-    function setEndCloseSale(uint256 newEndCloseSale) public onlyOwner {
-        endCloseSale = newEndCloseSale;
+    function setEndPreSale(uint256 newEndPreSale) public onlyOwner {
+        endPreSale = newEndPreSale;
+    }
+
+    // изменение даты начала основной распродажи
+    function setStartSale(uint256 newStartSale) public onlyOwner {
+        startPreSale = newStartSale;
+    }
+
+    // изменение даты окончания основной распродажи
+    function setEndSale(uint256 newEndSaled) public onlyOwner {
+        endPreSale = newEndSaled;
     }
 
     // Изменение адреса оператора бекэнда
@@ -116,7 +128,7 @@ contract CloseICO is Ownable {
     // Изменение курса доллра к эфиру
     function setBuyPrice(uint256 _dollar) public onlyOwner {
         dollarPrice = _dollar;
-        buyPrice = (1e17/dollarPrice)*4; // 0.4 usd
+        buyPrice = (1e17/dollarPrice)*21; // 2.10 usd
         emit UpdateDollar(now, dollarPrice);
     }
 
@@ -148,66 +160,80 @@ contract CloseICO is Ownable {
      * Payable's section
      */
 
-
+    function isPreSale() public constant returns(bool) {
+        return now >= startPreSale && now <= endPreSale;
+    }
 
     // callback функция контракта
     function () public payable {
         require(isWhitelisted(msg.sender));
-        if (isCloseSale()) {
-            closeSale(msg.sender, msg.value);
-            require(soldTokensCloseSale<=hardCapCloseSale);
-            investedEther[msg.sender] = investedEther[msg.sender].add(msg.value);
+        require(isPreSale());
+        preSale(msg.sender, msg.value);
+        require(soldTokens<=hardCapPreSale);
+        investedEther[msg.sender] = investedEther[msg.sender].add(msg.value);
+    }
+
+    // выпуск токенов в период предварительной распродажи
+    function preSale(address _investor, uint256 _value) internal {
+
+        // + 15 % all
+        uint256 tokens = _value.mul(1e18).div(buyPrice);
+        uint256 bonusSumTokens = tokens.mul(bonusSum(tokens)).div(100);
+        tokens = tokens.add(bonusSumTokens); // 88,5%
+        token.mintFromICO(_investor, tokens);
+
+        uint256 tokensTeam = tokens.mul(20).div(177); // 10% - осторожно цикличное число
+        token.mintFromICO(team, tokensTeam);
+
+        uint256 tokensBoynty = tokens.div(59); // 15/885  = 1,5%  -  - осторожно цикличное число
+        token.mintFromICO(bounty, tokensBoynty);
+
+        weisRaised = weisRaised.add(msg.value);
+        tokens = tokens.add(tokensTeam).add(tokensBoynty); // в расчет идут все
+        soldTokens = soldTokens.add(tokens);
+    }
+
+
+    function bonusSum(uint256 _amount) pure private returns(uint256) {
+        if (_amount > 350000*1e18) {
+            return 10;
+        } else if (_amount > 150000*1e18) {
+            return 7;
+        } else if (_amount > 50000*1e18) {
+            return 5;
+        } else if (_amount > 10000*1e18) {
+            return 3;
         } else {
-            revert();
+            return 0;
         }
     }
 
-    // проверка на то что сейл идет
-    function isCloseSale() public constant returns(bool) {
-        return now >= startCloseSale && now <= endCloseSale;
-    }
 
-    // выпуск токенов в период закрытой распродажи
-    function closeSale(address _investor, uint256 _valueEther) internal {
-        uint256 tokens = _valueEther.mul(1e18).div(buyPrice);
-        uint256 tokensBonus = tokens.mul(25).div(100);
-        tokens = tokens.add(tokensBonus);
-        soldTokensCloseSale = soldTokensCloseSale.add(distributionTokens(_investor, tokens));
-        weisRaised = weisRaised.add(_valueEther);
-    }
-
-    // Ручная отправка токенов
-    function mintManual(address receiver, uint256 _tokens) public backEnd {
-        token.transferFromICO(receiver, _tokens);
-
-        uint256 tokensTeam = _tokens.mul(2).div(7); // 33 % // 33 /60 11/20
-        token.transferFromICO(team, tokensTeam);
-
-        uint256 tokensBoynty = _tokens.div(30); // 2%
-        token.transferFromICO(bounty, tokensBoynty);
-
-        uint256 tokensReserve = _tokens.div(12); //  1/12 = 5%
-        token.transferFromICO(reserve, tokensReserve);
-        soldTokensCloseSale = soldTokensCloseSale.add(tokensTeam).add(tokensBoynty).add(tokensReserve);
-    }
-
-    function distributionTokens(address _recipient, uint256 _value) internal returns (uint256) {
-        token.transferFromICO(_recipient, _value);
-
-        uint256 tokensTeam = _value.mul(2).div(7); // 33 % // 33 /60 11/20
-        token.transferFromICO(team, tokensTeam);
-
-        uint256 tokensBoynty = _value.div(30); // 2%
-        token.transferFromICO(bounty, tokensBoynty);
-
-        uint256 tokensReserve = _value.div(12); //  1/12 = 5%
-        token.transferFromICO(reserve, tokensReserve);
-        uint256 sended = _value.add(tokensTeam).add(tokensBoynty).add(tokensReserve);
-        return sended;
-    }
 
     // Отправка эфира с контракта
     function transferEthFromContract(address _to, uint256 amount) public onlyOwner {
+        //require(weisRaised >= softCap);
         _to.transfer(amount);
+    }
+
+    /*******************************************************************************
+     * Refundable
+     */
+    function refundPreICO() public {
+        require(soldTokens < softCapPreSale && now > endPreSale);
+        uint rate = investedEther[msg.sender];
+        require(investedEther[msg.sender] >= 0);
+        investedEther[msg.sender] = 0;
+        msg.sender.transfer(rate);
+        weisRaised = weisRaised.sub(rate);
+    }
+
+    function refundMainICO() public {
+        require(weisRaised < softCapPreSale && now > endPreSale);
+        uint rate = investedEther[msg.sender];
+        require(investedEther[msg.sender] > 0);
+        investedEther[msg.sender] = 0;
+        msg.sender.transfer(rate);
+        weisRaised = weisRaised.sub(rate);
     }
 }
