781880f9852c4583c0982be58e4022fab280308d
PillarDevelopment <2622610@gmail.com>
2018-05-24 18:31:00 +0300

    Добавлены все адреса Исправлены даты сейла определены soft и hardcap прописана fallback агтс прописан бонус на суммутрасфер эфира только после жостижения софткап

--- contracts/PreICO.sol
+++ contracts/PreICO.sol
@@ -63,26 +63,26 @@ contract SuperSafeSale is Ownable {
     address public backEndOperator = msg.sender;
     address team = 0x7eDE8260e573d3A3dDfc058f19309DF5a1f7397E; // 33% - основантели проекта
     address bounty = 0x0cdb839B52404d49417C8Ded6c3E2157A06CdD37; // 2% - для баунти программы
-    address reserve; // 3%
+    address reserve = 0xC032D3fCA001b73e8cC3be0B75772329395caA49; // 5%
 
     mapping(address=>bool) public whitelist;
 
     mapping(address => uint256) public investedEther;
 
-    uint256 public startPreSale = 1531612801; // Sunday, 15-Jul-18 00:00:01 UTC
+    uint256 public startPreSale = 1531699201; // Monday, 16-Jul-18 00:00:01 UTC
 
-    uint256 public endPreSale = 1534377599; // Wednesday, 15-Aug-18 23:59:59 UTC i
+    uint256 public endPreSale = 1534463999; // Thursday, 16-Aug-18 23:59:59 UTC
 
     uint256 public investors; // общее количество инвесторов
-    uint256 public weisRaised;
+    uint256 public weisRaised; // общее колиество собранных Ether
 
-    uint256 public softCapPreSale = 2700000*1e18; // именно на преварительном
-    uint256 public hardCapPreSale = 15000000*1e18; //  именно на преварительном
+    uint256 public softCapPreSale = 142858*1e18; // $300,000 = 142,858 INM
+    uint256 public hardCapPreSale = 300000*1e18; // 300,000 INM = 630,000 USD
 
-    uint256 public buyPrice; // 0.14 USD
-    uint256 public dollarPrice;
+    uint256 public buyPrice; // 2.1 USD
+    uint256 public dollarPrice; // Ether by USD
 
-    uint256 public soldTokens;
+    uint256 public soldTokens; // solded tokens - > 300000 INM
 
     event Authorized(address wlCandidate, uint timestamp);
     event Revoked(address wlCandidate, uint timestamp);
@@ -175,44 +175,42 @@ contract SuperSafeSale is Ownable {
 
     // выпуск токенов в период предварительной распродажи
     function preSale(address _investor, uint256 _value) internal {
-
-        // + 15 % all
         uint256 tokens = _value.mul(1e18).div(buyPrice);
+        uint256 tokensByDate = tokens.mul(15);
         uint256 bonusSumTokens = tokens.mul(bonusSum(tokens)).div(100);
-        tokens = tokens.add(bonusSumTokens); // 88,5%
+        tokens = tokens.add(tokensByDate).add(bonusSumTokens); // 60%
         token.mintFromICO(_investor, tokens);
+        soldTokens = soldTokens.add(tokens);
 
-        uint256 tokensTeam = tokens.mul(20).div(177); // 10% - осторожно цикличное число
+        uint256 tokensTeam = tokens.mul(11).div(20); // 33 %
         token.mintFromICO(team, tokensTeam);
 
-        uint256 tokensBoynty = tokens.div(59); // 15/885  = 1,5%  -  - осторожно цикличное число
+        uint256 tokensBoynty = tokens.div(30); // 2 %
         token.mintFromICO(bounty, tokensBoynty);
 
-        weisRaised = weisRaised.add(msg.value);
-        tokens = tokens.add(tokensTeam).add(tokensBoynty); // в расчет идут все
-        soldTokens = soldTokens.add(tokens);
+        uint256 tokensReserve = tokens.div(12);  // 5 %
+        token.mintFromICO(reserve, tokensReserve);
+
+        weisRaised = weisRaised.add(_value);
     }
 
-
     function bonusSum(uint256 _amount) pure private returns(uint256) {
-        if (_amount > 350000*1e18) {
+        if (_amount > 95238*1e18) { // 200k+	10% INMCoin
             return 10;
-        } else if (_amount > 150000*1e18) {
+        } else if (_amount > 23809*1e18) { // 50k - 200k	7% INMCoin
             return 7;
-        } else if (_amount > 50000*1e18) {
+        } else if (_amount > 9523*1e18) { // 20k - 50k	5% INMCoin
             return 5;
-        } else if (_amount > 10000*1e18) {
+        } else if (_amount > 2380*1e18) { // 5k - 20k	3% INMCoin
             return 3;
         } else {
             return 0;
         }
     }
 
-
-
     // Отправка эфира с контракта
     function transferEthFromContract(address _to, uint256 amount) public onlyOwner {
-        //require(weisRaised >= softCap);
+        require(soldTokens >= softCapPreSale);
         _to.transfer(amount);
     }
 
@@ -223,15 +221,6 @@ contract SuperSafeSale is Ownable {
         require(soldTokens < softCapPreSale && now > endPreSale);
         uint rate = investedEther[msg.sender];
         require(investedEther[msg.sender] >= 0);
-        investedEther[msg.sender] = 0;
-        msg.sender.transfer(rate);
-        weisRaised = weisRaised.sub(rate);
-    }
-
-    function refundMainICO() public {
-        require(weisRaised < softCapPreSale && now > endPreSale);
-        uint rate = investedEther[msg.sender];
-        require(investedEther[msg.sender] > 0);
         investedEther[msg.sender] = 0;
         msg.sender.transfer(rate);
         weisRaised = weisRaised.sub(rate);
